70f5ab26432a51077407d362fa09878a898514b8
9Thiago <thiago_mr@live.com>
2022-07-06 16:33:25 -0300

    add paginação

--- src/main/java/daw2/trabalho/controller/EmprestimoController.java
+++ src/main/java/daw2/trabalho/controller/EmprestimoController.java
@@ -1,6 +1,14 @@
 package daw2.trabalho.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +21,8 @@ import daw2.trabalho.service.EmprestimoService;
 
 @Controller
 public class EmprestimoController {
+
+	private static final Logger logger = LoggerFactory.getLogger(EmprestimoController.class);
 
 	@Autowired
 	private EmprestimoRepository emprestimoRepository;
@@ -34,6 +44,21 @@ public class EmprestimoController {
 		emprestimoService.saveEmprestimo(emprestimo);
 		return mv;
 	}
-	
-	
+
+	@GetMapping("emprestimosp")
+	public ModelAndView pajiina(@PageableDefault(size = 4) Pageable pageable) {
+		ModelAndView mv = new ModelAndView("emprestimosp");
+		Page<Emprestimo> pagina = emprestimoRepository.findAll(pageable);
+		mv.addObject("pagina", pagina);
+		List<Integer> numerosPaginas = new ArrayList<>();
+		for (int cont = 1; cont <= pagina.getTotalPages(); cont++) {
+			numerosPaginas.add(cont);
+			mv.addObject("numerosPaginas", numerosPaginas);
+		}
+		logger.trace("===pajiinaa===: {}", pagina);
+		logger.trace("===pajiinaa===: {}", pagina.getContent());
+
+		return mv;
+	}
+
 }
